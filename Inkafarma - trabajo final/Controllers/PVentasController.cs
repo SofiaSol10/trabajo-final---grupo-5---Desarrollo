ee1528e1eeedce85d738f47190e71b834b50555d
SofiaSol10 <sofiasolarih10@gmail.com>
2025-09-24 13:16:59 -0500

    Se esta haciendo un prototipo de la vista "Puntos de Venta"

---
+++ Inkafarma - trabajo final/Controllers/PVentasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Inkafarma___trabajo_final.Controllers
+{
+    public class PVentasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
